95213aa6b2b1dfc3440bc78fd85011bb0a3eac38
suprita shankar <suprita.shankar@gmail.com>
2016-10-08 14:18:52 -0700

    clean up the messenger

--- messenger/models.py
+++ messenger/models.py
@@ -1,16 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-	full_name = models.CharField(max_length=30)
+	GENDER_CHOICES = [(0, 'Male'), (1, 'Female')]
+
+	user = models.OneToOneField(User, unique=True)
 	school = models.CharField(max_length = 30)
 	email = models.EmailField(max_length = 20)
 	password = models.CharField(max_length = 15)
 	date_of_birth = models.DateField(max_length = 8)
-	GENDER_CHOICES = [(0, 'Male'), (1, 'Female')]
-	gender = models.IntegerField(choices=GENDER_CHOICES)	
+	gender = models.IntegerField(choices=GENDER_CHOICES)
 	facebook_uid = models.CharField(max_length = 30)
 	score = models.IntegerField(default=1)
-
