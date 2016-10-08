c8fd48883564b7c3825a186479655782b2969f9f
suprita shankar <suprita.shankar@gmail.com>
2016-10-08 12:41:00 -0700

    add urls and campus

--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import pusher
+import json
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.views.generic.base import TemplateView
+from django.views import View
 
-# Create your views here.
+class HomeView(TemplateView):
+
+    template_name = "messenger/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        return context
